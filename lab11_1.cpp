aac2e737c0584ac9fda47df2dfa19076231539b5
lab6 <chaidan254905@gmail.com>
2026-01-05 17:04:50 +0700

    lap11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,29 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+
+
+int main() {
+
+    cout << "Press Enter 3 times to reveal your future."<<endl;
+
+    for (int i = 0 ; i< 3 ; i++) {
+        cin.get();
+    }
+    
+    srand(time(0));
+    
+    string grade[] = { "A" , "B+" , "B" ,"C+" , "C" , "D+" , "D" , "F" , "W"};
+    
+    int random = rand() % 9;
+    
+     cout << "You will get " <<grade[random] <<" in this 261102." << endl;
+
+
+
+
+}
